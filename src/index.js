68bbd3ff80bb89621c513fcb5fb4da3245124fc1
charles161 <fortloop.charles@gmail.com>
2020-06-21 13:57:20 +0530

    Added prisma client

--- src/index.js
+++ src/index.js
@@ -1,22 +1,44 @@
 const { GraphQLServer } = require("graphql-yoga");
 
-// 1
-const typeDefs = `
-type Query {
-  info: String!
-}
-`;
-
+let links = [
+  {
+    id: "link-0",
+    url: "www.howtographql.com",
+    description: "Fullstack tutorial for GraphQL"
+  }
+];
+let idCount = links.length;
 // 2
 const resolvers = {
   Query: {
-    info: () => `This is the API of a Hackernews Clone`
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links
+  },
+  Link: {
+    //these are automatically resolved, so no need of them
+    id: parent => parent.id,
+    description: parent => parent.description,
+    url: parent => parent.url
+  },
+  Mutation: {
+    // 2
+    post: (parent, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url
+      };
+      links.push(link);
+      return link;
+    }
   }
 };
 
 // 3
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: "./src/schema.graphql",
   resolvers
 });
 server.start(() => console.log(`Server is running on http://localhost:4000`));
+
+//https://www.howtographql.com/graphql-js/4-adding-a-database/
