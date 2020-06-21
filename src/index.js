32800b3e5afc2f927954ac4a4d0be2fb10e9c778
charles161 <fortloop.charles@gmail.com>
2020-06-21 15:28:33 +0530

    Added Real time subscriptions

--- src/index.js
+++ src/index.js
@@ -1,43 +1,30 @@
 const { GraphQLServer } = require("graphql-yoga");
-
-let links = [
-  {
-    id: "link-0",
-    url: "www.howtographql.com",
-    description: "Fullstack tutorial for GraphQL"
-  }
-];
-let idCount = links.length;
+const { prisma } = require("./generated/prisma-client");
+const Query = require("./resolvers/Query");
+const Mutation = require("./resolvers/Mutation");
+const User = require("./resolvers/User");
+const Link = require("./resolvers/Link");
+const Subscription = require("./resolvers/Subscription");
 // 2
 const resolvers = {
-  Query: {
-    info: () => `This is the API of a Hackernews Clone`,
-    feed: () => links
-  },
-  Link: {
-    //these are automatically resolved, so no need of them
-    id: parent => parent.id,
-    description: parent => parent.description,
-    url: parent => parent.url
-  },
-  Mutation: {
-    // 2
-    post: (parent, args) => {
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url
-      };
-      links.push(link);
-      return link;
-    }
-  }
+  Query,
+  Mutation,
+  Subscription,
+
+  User,
+  Link
 };
 
 // 3
 const server = new GraphQLServer({
   typeDefs: "./src/schema.graphql",
-  resolvers
+  resolvers,
+  context: request => {
+    return {
+      ...request,
+      prisma
+    };
+  }
 });
 server.start(() => console.log(`Server is running on http://localhost:4000`));
 
